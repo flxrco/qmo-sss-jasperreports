be676c3979b422171b224f5b40e574a97bb10f9e
Felix <jonfelixrico@gmail.com>
2018-02-18 23:26:32 +0800

    added the Oracle JDBC driver and renamed a few folders

--- src/jasperreports/UniversalGenerator.java
+++ src/jasperreports/UniversalGenerator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jasperrports;
+package jasperreports;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
