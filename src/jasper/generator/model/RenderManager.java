5f8bd94d1461638de5c1d60b9c863daa6b78ec6e
Felix <jonfelixrico@gmail.com>
2018-03-02 18:32:58 +0800

    edited the RenderManager class and created the ExportManager class

--- src/jasper/generator/model/RenderManager.java
+++ src/jasper/generator/model/RenderManager.java
@@ -7,6 +7,7 @@ package jasper.generator.model;
 
 import jasper.generator.model.exceptions.EmptyRenderQueueException;
 import java.io.File;
+import java.sql.Connection;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -18,7 +19,7 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 
 /**
- *
+ * The manager for generating reports.
  * @author Felix
  */
 public class RenderManager {
@@ -45,7 +46,14 @@ public class RenderManager {
         }
     }
     
-    public Map<String, Object> render() throws JRException, EmptyRenderQueueException {
+    /**
+     * Generates the report that on the start of the queue. The dataset to be used is from the Connection argument
+     * @param con the Connection object to be used to fetch the dataset
+     * @return the Map used as the parameters for the JasperFillManager
+     * @throws JRException if a Jaspersoft-related error was encountered
+     * @throws EmptyRenderQueueException if this method was called even though the queue is already empty
+     */
+    public Map<String, Object> render(Connection con) throws JRException, EmptyRenderQueueException {
         Map<String, Object> vals = new HashMap<>(); //map to be passed to the JasperPrint
         String[] tempArgs = this.args.poll(); //dequeues and then passes the values to the tempargs
         String[] tempParams = compiler.params; //passes the param names from the compilemanager
@@ -61,7 +69,7 @@ public class RenderManager {
         
         vals.put("SUBREPORT_DIR", compiler.subreportPath); //the .jrxml requires this as a parameter. this is where it looks for its subreports
         
-        JasperPrint report = JasperFillManager.fillReport(compiler.report, vals, compiler.con); //'render' the report with the values
+        JasperPrint report = JasperFillManager.fillReport(compiler.report, vals, con); //'render' the report with the values
         
         String split[] = dest.split("/");
         new File(String.join("/", Arrays.copyOfRange(split, 0, split.length - 1))).mkdirs();
@@ -73,6 +81,27 @@ public class RenderManager {
         vals.put("RENDER_NO", ++rendered);
         
         return vals;
+    }
+    
+    /**
+     * Generates the report that on the start of the queue. It will use the Connection from the CompileManager to fetch the dataset
+     * @return the Map used as the parameters for the JasperFillManager
+     * @throws JRException JRException if a Jaspersoft-related error was encountered
+     * @throws EmptyRenderQueueException if this method was called even though the queue is already empty
+     */
+    public Map<String, Object> render() throws JRException, EmptyRenderQueueException {
+        return render(compiler.con);
+    }
+    
+    /**
+     * Generates all of the reports that are in the queue. It uses the Connection from the CompileManager to fetch the dataset.
+     * @throws JRException JRException if a Jaspersoft-related error was encountered
+     * @throws EmptyRenderQueueException if this method was called even though the queue is already empty
+     */
+    public void renderAll() throws JRException, EmptyRenderQueueException {
+        for (int i = 0; i < args.size(); i++) {
+            render();
+        }
     }
     
     public int countInitial() {
